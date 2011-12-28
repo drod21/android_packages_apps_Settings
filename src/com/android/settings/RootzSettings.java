791648c588345cb711511e3cfdc9bd5928a1e8a2
xoomdev <xoomdevnet@gmail.com>
2011-12-28 11:43:51 -0600

    Disable Launcher Settings if our Launcher is not set as default to avoid FC on Settings. Why anyone would touch the settings if the Launcher is not installed or default is beyond me. But it has been brought to my attn.

--- src/com/android/settings/RootzSettings.java
+++ src/com/android/settings/RootzSettings.java
@@ -1,9 +1,13 @@
 package com.android.settings;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.ActivityInfo;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.content.pm.PackageManager;
 
 import java.util.List;
 
@@ -17,7 +21,24 @@ public class RootzSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rootz_settings);
-
         mLauncherPrefs = (PreferenceScreen) findPreference(LAUNCHER_PREFS);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.MAIN");
+        intent.addCategory("android.intent.category.HOME");
+        ActivityInfo a = getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY).activityInfo;
+        if (a != null && a.name.equals("com.android.launcher2.Launcher") && (a.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0 ){
+        if ( findPreference(LAUNCHER_PREFS) == null){
+                findPreference(LAUNCHER_PREFS).setEnabled(true);
+            }
+        } else {
+            if ( findPreference(LAUNCHER_PREFS) != null){
+                findPreference(LAUNCHER_PREFS).setEnabled(false);
+            }
+        }
     }
 }
