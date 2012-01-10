45a77a655e6cd8b581d5036838aac1e39bacf7c9
xoomdev <xoomdevnet@gmail.com>
2012-01-10 04:12:47 -0600

    Add settings for navigation bar color (romanbb)

--- src/com/android/settings/NavigationBarSettings.java
+++ src/com/android/settings/NavigationBarSettings.java
@@ -1,6 +1,7 @@
 package com.android.settings;
 import com.android.settings.R;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -8,15 +9,18 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 public class NavigationBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String SHOW_MENU_BUTTON = "show_menu_button";
     private static final String SHOW_SEARCH_BUTTON = "show_search_button";
     private static final String USE_ALT_ICONS = "use_alt_icons";
+    private static final String NAVIGATION_BUTTON_COLOR = "navigation_button_color";
     private CheckBoxPreference mShowMenuButton;
     private CheckBoxPreference mShowSearchButton;
     private CheckBoxPreference mUseAltIcons;
+    private ColorPickerPreference mNavButtonColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,9 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         mUseAltIcons = (CheckBoxPreference) prefSet.findPreference(USE_ALT_ICONS);
         mUseAltIcons.setChecked(Settings.System.getInt(getContentResolver(),
             Settings.System.USE_ALT_ICONS, 0) == 1);
+
+        mNavButtonColor = (ColorPickerPreference) prefSet.findPreference(NAVIGATION_BUTTON_COLOR);
+        mNavButtonColor.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -58,7 +65,16 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         return false;
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNavButtonColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                .valueOf(newValue)));
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BUTTON_COLOR, intHex);
+            return true;
+        }
         return false;
     }
 }
