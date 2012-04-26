8d9f770184a8e1ed2b57d49aab1151d705842231
Derek Rodriguez <drod2169@gmail.com>
2012-04-25 23:26:09 -0400

    Add Clock settings to StatusBarSettings
    Conflicts:
    
    	res/values/strings.xml

--- src/com/android/settings/StatusBarSettings.java
+++ src/com/android/settings/StatusBarSettings.java
@@ -3,6 +3,7 @@ import com.android.settings.R;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
@@ -12,7 +13,12 @@ import android.provider.Settings;
 public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String BATTERY_PERCENTAGES = "battery_percentages";
+    private static final String PREF_CLOCK_DISPLAY_STYLE = "clock_am_pm";
+    private static final String PREF_CLOCK_STYLE = "clock_style";
+
     private CheckBoxPreference mBatteryPercentages;
+    private ListPreference mAmPmStyle;
+    private ListPreference mClockStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,18 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
         mBatteryPercentages = (CheckBoxPreference) prefSet.findPreference(BATTERY_PERCENTAGES);
         mBatteryPercentages.setChecked(Settings.System.getInt(getContentResolver(),
             Settings.System.BATTERY_PERCENTAGES, 0) == 1);
+        mClockStyle = (ListPreference) prefSet.findPreference(PREF_CLOCK_STYLE);
+        mAmPmStyle = (ListPreference) prefSet.findPreference(PREF_CLOCK_DISPLAY_STYLE);
+
+        int styleValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_AM_PM, 2);
+        mAmPmStyle.setValueIndex(styleValue);
+        mAmPmStyle.setOnPreferenceChangeListener(this);
+
+        int clockVal = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CLOCK, 1);
+        mClockStyle.setValueIndex(clockVal);
+        mClockStyle.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -37,6 +55,17 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+         if (preference == mAmPmStyle) {
+            int statusBarAmPm = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
+            return true;
+        } else if (preference == mClockStyle) {
+            int val = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CLOCK, val);
+            return true;
+        }
         return false;
     }
 }
