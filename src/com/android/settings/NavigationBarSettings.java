0b91454172ce0093832d80cc72671b6e8ce22c41
Derek Rodriguez <drod2169@gmail.com>
2012-04-25 23:30:56 -0400

    Add ability to select different items for outside slots
    Conflicts:
    
    	res/values/strings.xml
    	src/com/android/settings/NavigationBarSettings.java

--- src/com/android/settings/NavigationBarSettings.java
+++ src/com/android/settings/NavigationBarSettings.java
@@ -4,6 +4,7 @@ import com.android.settings.R;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
@@ -17,6 +18,12 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
     private static final String SHOW_SEARCH_BUTTON = "show_search_button";
     private CheckBoxPreference mShowMenuButton;
     private CheckBoxPreference mShowSearchButton;
+    private static final String NAV_BUTTONS_SLOT_ONE = "nav_buttons_slot_one";
+    private static final String NAV_BUTTONS_SLOT_FIVE = "nav_buttons_slot_five";
+    private static final String USE_ALT_ICONS = "use_alt_icons";
+    private static final String NAVIGATION_BUTTON_COLOR = "navigation_button_color";
+    private ListPreference mSlotOne;
+    private ListPreference mSlotFive;
     private ColorPickerPreference mNavButtonColor;
 
     @Override
@@ -25,13 +32,17 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.navigation_bar_settings);
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        mShowMenuButton = (CheckBoxPreference) prefSet.findPreference(SHOW_MENU_BUTTON);
-        mShowMenuButton.setChecked(Settings.System.getInt(getContentResolver(),
-            Settings.System.SHOW_MENU_BUTTON, 0) == 1);
+        mSlotOne = (ListPreference) prefSet.findPreference(NAV_BUTTONS_SLOT_ONE);
+        int slotOneValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.NAV_BUTTONS_SLOT_ONE, 0);
+        mSlotOne.setValueIndex(slotOneValue);
+        mSlotOne.setOnPreferenceChangeListener(this);
 
-        mShowSearchButton = (CheckBoxPreference) prefSet.findPreference(SHOW_SEARCH_BUTTON);
-        mShowSearchButton.setChecked(Settings.System.getInt(getContentResolver(),
-            Settings.System.SHOW_SEARCH_BUTTON, 0) == 1);
+        mSlotFive = (ListPreference) prefSet.findPreference(NAV_BUTTONS_SLOT_FIVE);
+        int slotFiveValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.NAV_BUTTONS_SLOT_FIVE, 0);
+        mSlotFive.setValueIndex(slotFiveValue);
+        mSlotFive.setOnPreferenceChangeListener(this);
 
         mNavButtonColor = (ColorPickerPreference) prefSet.findPreference(NAVIGATION_BUTTON_COLOR);
         mNavButtonColor.setOnPreferenceChangeListener(this);
@@ -61,6 +72,16 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BUTTON_COLOR, intHex);
+            return true;
+        } else if (preference == mSlotOne) {
+            int valOne = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NAV_BUTTONS_SLOT_ONE, valOne);
+            return true;
+        } else if (preference == mSlotFive) {
+            int valFive = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NAV_BUTTONS_SLOT_FIVE, valFive);
             return true;
         }
         return false;
